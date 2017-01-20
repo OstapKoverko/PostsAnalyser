d083a346641d4aae0df4f7f753015e6a008521cc
Ostap <tank302@gmail.com>
2017-01-20 13:32:43 +0000

    Fix bugs

--- app/Posts/app.js
+++ app/Posts/app.js
@@ -2,7 +2,7 @@ var app = angular.module('myApp', ["ngRoute"]);
 
 app.config(function($routeProvider) {
   $routeProvider  
-  .when("/posts", {
+  .when("/postsList", {
     templateUrl : "posts.html",
     controller : "postsController"
   })  
