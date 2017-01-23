7067e4d84e414bfc5e98aaae060e424b884d3340
Ostap <tank302@gmail.com>
2017-01-23 07:18:54 +0000

    Release v.01

--- app/Posts/PostsService.js
+++ app/Posts/PostsService.js
@@ -1,31 +1,31 @@
 app.service('PostsService', function($http) { 
-  this.getPosts = function (callback, callbackError) {
-    $http.get("https://jsonplaceholder.typicode.com/posts/").then(
-      function (response) {
-        callback(response.data);    
-      },
-      function (response) {
-        callbackError("GetPosts method's status: " + response.status + " " + response.statusText); 
-      }
-    );
-  };
-  this.getPostById = function (id, callback, callbackError) {
-    $http.get("https://jsonplaceholder.typicode.com/posts/" + id).then(
-      function (response) {
-        callback(response.data);
-      },
-      function (response) {
-        callbackError("GetPostById method's status: " + response.status + " " + response.statusText);
-      });
-  };
-  this.getCommentsByPostId = function (postId, callback, callbackError) {
-    $http.get("https://jsonplaceholder.typicode.com/posts/" + postId + "/comments").then(
-      function (response) {
-        callback(response.data);    
-      }, 
-      function (response) {
-        callbackError("GetCommentsByPostId method's status: " + response.status + " " + response.statusText);
-      }
-    );
-  };  
+	this.getPosts = function (callback, callbackError) {
+		$http.get("https://jsonplaceholder.typicode.com/posts/").then(
+			function (response) {
+				callback(response.data);    
+			},
+			function (response) {
+				callbackError("GetPosts method's status: " + response.status + " " + response.statusText); 
+			}
+		);
+	};
+	this.getPostById = function (id, callback, callbackError) {
+		$http.get("https://jsonplaceholder.typicode.com/posts/" + id).then(
+			function (response) {
+				callback(response.data);
+			},
+			function (response) {
+				callbackError("GetPostById method's status: " + response.status + " " + response.statusText);
+			});
+	};
+	this.getCommentsByPostId = function (postId, callback, callbackError) {
+		$http.get("https://jsonplaceholder.typicode.com/posts/" + postId + "/comments").then(
+			function (response) {
+				callback(response.data);    
+			}, 
+			function (response) {
+				callbackError("GetCommentsByPostId method's status: " + response.status + " " + response.statusText);
+			}
+		);
+	};  
 });
