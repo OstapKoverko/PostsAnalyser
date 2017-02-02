7c0ceca3360a5b511d69eaa3f88259a9a3883c43
Ostap <tank302@gmail.com>
2017-02-02 15:17:35 +0000

    Release v.5.0

--- app/Posts/PostController.js
+++ app/Posts/PostController.js
@@ -1,17 +1,23 @@
 app.controller('postController', function($scope, $routeParams, PostsService){
 	// USE PROMISES
+	$scope.formEditable = false;
+	$scope.editPost = function () {
+		$scope.formEditable = true;
+	};
+	$scope.cancelEditPost = function () {
+		$scope.formEditable = false;
+	};
 	$scope.savePost = function (postForm) {
-		$scope.data = {
-			id: $scope.post.id,
-			userId: $scope.post.userId,
-			title: $scope.post.title,
-			body: $scope.post.body
-		};
 		console.log("$scope.savePost - done!");
 		console.log($scope.data);
 		console.log("Is form valid? - " + postForm.$valid);
 		if (postForm.$valid) {
-			PostsService.savePost($scope.post.id, $scope.data).then(
+			PostsService.savePost($scope.post.id, {
+				id: $scope.post.id,
+				userId: $scope.post.userId,
+				title: $scope.post.title,
+				body: $scope.post.body
+			}).then(
 				function onSuccess(response) {
 					console.log("Save post status: " + response.status + " " + response.statusText);
 					$scope.formEditable = false;
@@ -22,7 +28,7 @@ app.controller('postController', function($scope, $routeParams, PostsService){
 			return;
 		} console.log("Form is invalid !!!");
 	};
-	// Юра а між викликами сервісів не потрібно зробити пропуск для візуального розділення частин контролера?
+
 	PostsService.getPostById($routeParams.id).then(
 		function onSuccess(response) {
 			$scope.post = response.data;
@@ -32,7 +38,7 @@ app.controller('postController', function($scope, $routeParams, PostsService){
 			$scope.postErrorMessage = "GetPostById method's status: " + response.status + " " + response.statusText;
 		}
 	);
-	// Юра а між викликами сервісів не потрібно зробити пропуск для візуального розділення частин контролера?
+
 	PostsService.getCommentsByPostId($routeParams.id).then(
 		function onSuccess(response) {
 			$scope.comments = response.data;
@@ -43,12 +49,4 @@ app.controller('postController', function($scope, $routeParams, PostsService){
 			$scope.commentsErrorMessage = "GetCommentsByPostId method's status: " + response.status + " " + response.statusText;
 		}
 	);
-	// Юра а між викликами сервісів не потрібно зробити пропуск для візуального розділення частин контролера?
-	$scope.formEditable = false;
-	$scope.editPost = function () {
-		$scope.formEditable = true;
-	};
-	$scope.cancelEditPost = function () {
-		$scope.formEditable = false;
-	};
 });
--- app/Posts/app.js
+++ app/Posts/app.js
@@ -2,6 +2,10 @@ var app = angular.module('myApp', ["ngRoute"]);
 
 app.config(function($routeProvider) {
   $routeProvider  
+    .when("/", {
+      templateUrl : "app/Posts/posts.html",
+      controller : "postsController"
+    }) 
     .when("/posts", {
       templateUrl : "app/Posts/posts.html",
       controller : "postsController"
