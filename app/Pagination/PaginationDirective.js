d478f277916c086cab8d29556c809bb97c2b4912
Ostap <tank302@gmail.com>
2017-02-15 08:56:58 +0000

    Release v.5.10

--- app/Pagination/PaginationDirective.js
+++ app/Pagination/PaginationDirective.js
@@ -4,15 +4,18 @@ app.directive("pagination", function() {
 		transclude: true,
 		// replace: true,
 		scope: {
+			items: "",
+			totallItems: "",
+			currentPage: "",
+			onSelectPage: "",
 			pages: "=directivePages"
 		},
 		templateUrl: "app/Pagination/_pagination.html",
 		controller: ["$scope",
 			function ($scope) {
-				$scope.pageSize = items,
-				$scope.postsLength = totallItems,
-				$scope.pageNumber = currentPage,
-				// Чому тут не можна оголошувати змінні ?
+				var items = $scope.pageSize;
+				var totallItems = $scope.postsLength;
+				var currentPage = $scope.pageNumber;
 				var pages = [];
 				var i = 1;
 				var k = totallItems / items;
