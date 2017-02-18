6913493022e4c6767ef00b89f0545ac8f2b892e4
Ostap <tank302@gmail.com>
2017-02-18 09:47:24 +0000

    Release v.5.18

--- app/Pagination/PaginationDirective.js
+++ app/Pagination/PaginationDirective.js
@@ -1,12 +1,10 @@
 app.directive("pagination", function() {
 	return {
 		restrict: "E",
-		transclude: true,
-		// replace: true,
 		scope: {
 			pageSize: "=",
 			postsQuantity: "=",
-			onSelectPage: "&"
+			onSelectPage: "="
 		},
 		templateUrl: "app/Pagination/pagination.html",
 		controller: ["$scope",
@@ -20,17 +18,8 @@ app.directive("pagination", function() {
 							pages.unshift(i);
 						}
 						$scope.pages = pages;
+						debugger;
 					}
-					// function refresh(pageSize) {
-					// 	if(!pageSize)return;
-					// 	var pages = [];
-					// 	var i = $scope.postsQuantity / pageSize;
-					// 	while (i > 0) {
-					// 		pages.unshift(i);
-					// 		i--;
-					// 	}
-					// 	$scope.pages = pages;
-					// }
 				);
 				$scope.selectPage = function (currentPage) {
 					$scope.currentPage = currentPage;
--- app/Posts/PostsController.js
+++ app/Posts/PostsController.js
@@ -1,6 +1,8 @@
 app.controller('postsController', function($scope, PostsService) {
 	$scope.pageSize = 10;
-	$scope.pageNumber = 1;
+	$scope
+	// $scope.pageNumber = 1;
+
 	$scope.setPageSize = function (pageSize) {
 		$scope.pageSize = pageSize;
 		getPostsService();
@@ -9,11 +11,7 @@ app.controller('postsController', function($scope, PostsService) {
 		$scope.pageNumber = pageNumber;
 		getPostsService();
 	};
-	// $scope.setPageNumber = function (pageNumber) {
-	// 	$scope.pageNumber = pageNumber;
-	// 	getPostsService();
-	// };
-	
+		getPostsService();
 	// USE STANDART CALLBACK
 	function getPostsService () {
 		PostsService.getPosts($scope.pageSize, $scope.pageNumber,
@@ -26,80 +24,9 @@ app.controller('postsController', function($scope, PostsService) {
 				$scope.postsErrorMesage = null;
 				$scope.posts = result.posts;
 				$scope.postsQuantity = result.postsQuantity;
-				
-	
-				
-				// // Створюємо масив з номерами сторінок
-				// var pages = [];
-				// var i = 1;
-				// var k = result.postslength / $scope.pageSize;
-				// for (i; i <= k; i++) {
-				// 	pages.push(i);
-				// }
-				// $scope.pages = pages;
-				// $scope.posts = result.posts;
+				debugger;
 			}
 		);
 	}
-
 	getPostsService();
 });
-
-
-	/////////////////////////////
-	// 2017.02.07  3:44 AM (!) //   
-	/////////////////////////////
-	
-	// // var pageNumber =2;
-	// var lastId;
-	// var firstId;
-
-	// $scope.setPageSize = function (pageSize) {
-	// 	console.log("pageSize = " + pageSize);
-	// 	$scope.createPages(pageSize);
-	// 	$scope.spliceAllPosts(0);
-	// 	pageSize = 0;
-	// };	
-
-	// $scope.setPageNumber = function(pageNumber) {
-	// 	console.log("pageNumber = " + pageNumber);
-	// 	lastId = $scope.pageSize * pageNumber;
- //   firstId = lastId - ($scope.pageSize - 1);
-	// 	// $scope.posts = $scope.posts.splice($scope.firstId, pageSize);
-	// 	// console.log($scope.posts);
-	// 	$scope.spliceAllPosts(firstId);
-	// 	firstId = 0;
-	// 	lastId = 0;
-	// };
-	
-	
-	// // Створюємо масив з номерами сторінок
-	// $scope.createPages = function () {
-	// 	$scope.pages = [];
-	// 	for (var i = 1; i <= $scope.allPosts.length / $scope.pageSize ; i++) {
-	// 		$scope.pages.push(i); 
-	// 	}
-	// };
-		
-
-	// // Вирізаємо потрібний фрагмент даних 
-	// $scope.spliceAllPosts = function (firstId) {
-	// 	$scope.posts = $scope.allPosts.splice(firstId, $scope.pageSize);	
-	// };
-	
-	
-	// // USE STANDART CALLBACK
-	// PostsService.getPosts(function (err, result) {
-	// 	if (err) {
-	// 		console.log(err);
-	// 		$scope.postsErrorMesage = err;
-	// 		return;
-	// 	}
-	// 		$scope.postsErrorMesage = null;
-	// 		$scope.allPosts = result;
-	// 		$scope.createPages(10);
-	// 		$scope.spliceAllPosts(0);
-	// 		// // console.log("Begin getPosts from post id " + firstId + " to post id " + lastId);
-	// 		// console.log("pageSize = " + pageSize);
-	// 		// console.log("$scope.pages = " + $scope.pages);
-	// });
