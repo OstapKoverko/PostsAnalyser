ee9938e1d625bd40ffcffcb18b3c11d981df5414
Ostap <tank302@gmail.com>
2017-03-20 14:49:56 +0000

    Release v.8.6

--- app/styleSwitcherProvider.js
+++ app/styleSwitcherProvider.js
@@ -1,17 +1,16 @@
 app.provider("styleSwitcher", function() {
-	var url = window.location.href;
 	function getParameterByName(name) {
 		name = name.replace(/[\[\]]/g, "\\$&");
-    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)");
-    var results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, " "));
+		var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)");
+		var results = regex.exec(window.location.href);
+		if (!results) return null;
+		if (!results[2]) return '';
+		return decodeURIComponent(results[2].replace(/\+/g, " "));
 	}
-  return {
-    framework: getParameterByName('framework'),
-    $get: function () {
-    	return getParameterByName('framework');
-    }
-  };			 
+	return {
+		framework: getParameterByName('framework'),
+		$get: function () {
+			return getParameterByName('framework');
+		}
+	};			 
 });	
